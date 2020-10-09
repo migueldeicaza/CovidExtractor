10ffd6ab3e2a82b5141345018424f949fd39a2b1
Miguel de Icaza <miguel@gnome.org>
2020-10-08 22:09:11 -0400

    Add

---
+++ CovidExtractor/CovidData.swift
@@ -0,0 +1,158 @@
+//
+//  CovidData.swift
+//  SharedCode
+//
+//  Created by Miguel de Icaza on 10/7/20.
+//
+
+import Foundation
+
+let formatVersion = 1
+
+// Imported from the Json file
+public struct TrackedLocation: Codable {
+    public var admin: String!
+    public var proviceState: String!
+    public var countryRegion: String!
+    public var lat, long: String!
+}
+
+// Imported from the JSON file, tends to have the last N samples (20 or so)
+// the last element is the current status, the delta is the difference
+// between the last two elements
+public struct Snapshot: Codable {
+    public var lastDeaths: [Int]!
+    public var lastConfirmed: [Int]!
+}
+
+public struct GlobalData: Codable {
+    public var time: Date = Date()
+    public var version: Int = formatVersion
+    public var globals: [String:TrackedLocation] = [:]
+}
+
+public struct SnapshotData: Codable {
+    public var time: Date = Date()
+    public var version: Int = formatVersion
+    public var snapshots: [String:Snapshot] = [:]
+}
+
+public struct Stats {
+    public var caption: String
+    public var totalCases, deltaCases: String
+    public var cases: [Int]
+    public var totalDeaths, deltaDeaths: String
+    public var deaths: [Int]
+}
+
+//public var us_ma_Stat = Stats (caption: "Massachussets", totalCases: "135k", deltaCases: "+644", deaths: "9,530", deltaDeaths: "+3")
+//public var us_Stat = Stats (caption: "United States", totalCases: "7.48M", deltaCases: "+34,491", deaths: "210k", deltaDeaths: "+332")
+//public var world_Stat = Stats (caption: "Worldwide", totalCases: "35.4M", deltaCases: "", deaths: "1.04M", deltaDeaths: "")
+
+
+func loadGlobalData (data: Data) -> GlobalData?
+{
+    let d = JSONDecoder()
+    return try? d.decode(GlobalData.self, from: data)
+}
+
+func loadSnapshotData (data: Data) -> SnapshotData?
+{
+    let d = JSONDecoder()
+    return try? d.decode(SnapshotData.self, from: data)
+}
+
+func load () -> (GlobalData, SnapshotData)?
+{
+    if let gd = try? Data(contentsOf: URL (fileURLWithPath: "/tmp/global")) {
+        if let id = try? Data (contentsOf: URL (fileURLWithPath: "/tmp/individual")){
+            if let lgd = loadGlobalData(data: gd) {
+                if let lsd = loadSnapshotData(data: id) {
+                    return (lgd, lsd)
+                }
+            }
+        }
+    }
+    return nil
+}
+
+var gd: GlobalData!
+var sd: SnapshotData!
+
+var emptyStat = Stats(caption: "Taxacussets", totalCases: "1234", deltaCases: "+11", cases: [], totalDeaths: "897", deltaDeaths: "+2", deaths: [])
+var fmt: NumberFormatter!
+
+func fmtLarge (_ n: Int) -> String
+{
+    
+    switch n {
+    case let x where x < 0:
+        return "0."     // "0." as a flag to determine something went wrong
+        
+    case 0..<99999:
+        return fmt.string(from: NSNumber (value: n)) ?? "?"
+        
+    case 100000..<999999:
+        return fmt.string(from: NSNumber (value: Float (n)/1000.0)) ?? "?" + "k"
+        
+    default:
+        return fmt.string(from: NSNumber (value: Float (n)/1000000.0)) ?? "?" + "M"
+    }
+}
+
+func fmtDelta (_ n: Int) -> String
+{
+    
+    switch n {
+    case let x where x < 0:
+        return "-0"     // "-0" as a flag to determine something went wrong
+        
+    case 0..<999:
+        return "+" + (fmt.string(from: NSNumber (value: n)) ?? "?")
+        
+    case 1000..<999999:
+        return "+" + (fmt.string(from: NSNumber (value: Float (n)/1000.0)) ?? "?") + "k"
+        
+    default:
+        return "+" + (fmt.string(from: NSNumber (value: Float (n)/1000000.0)) ?? "?") + "M"
+    }
+}
+
+func initDataTools ()
+{
+    fmt = NumberFormatter()
+    fmt.numberStyle = .decimal
+}
+
+public func fetch (code: String) -> Stats
+{
+    if gd == nil || sd == nil {
+        initDataTools ()
+        if let (a, b) = load () {
+            gd = a
+            sd = b
+        } else {
+            return emptyStat
+        }
+    }
+    //let tl = gd.globals [code]
+    guard let snapshot = sd.snapshots [code] else {
+        return emptyStat
+    }
+    
+    let last2Deaths = Array (snapshot.lastDeaths.suffix(2))
+    let totalDeaths = last2Deaths[1]
+    let deltaDeaths = last2Deaths[1]-last2Deaths[0]
+    let last2Cases = Array(snapshot.lastConfirmed.suffix(2))
+    let totalCases = last2Cases [1]
+    let deltaCases = last2Cases[1]-last2Cases[0]
+    return Stats (caption: code,
+                  totalCases: fmtLarge (totalCases),
+                  deltaCases: fmtDelta (deltaCases),
+                  cases: snapshot.lastDeaths,
+                  totalDeaths: fmtLarge (totalDeaths),
+                  deltaDeaths: fmtDelta (deltaDeaths),
+                  deaths: snapshot.lastDeaths)
+}
+
+
---
+++ CovidExtractor/main.swift
@@ -0,0 +1,147 @@
+//
+//  main.swift
+//  CovidExtractor
+//
+//  Created by Miguel de Icaza on 10/8/20.
+//
+
+import Foundation
+import SwiftCSV
+
+func populateDeathUS (slot: inout TrackedLocation, row: [String])
+{
+    slot.admin = row [5]
+    slot.proviceState = row [6]
+    slot.countryRegion = row [7]
+    slot.lat = row [8]
+    slot.long = row [9]
+}
+
+func extractUS (_ n: Int, _ fdeaths: CSV, _ fcases: CSV)
+{
+    // Now populate the data
+    for r in fdeaths.enumeratedRows {
+        let key = r [4] // fips
+        
+        if key == "" {
+            continue
+        }
+        var slot = gd.globals [key] ?? TrackedLocation ()
+        populateDeathUS(slot: &slot, row: r)
+        gd.globals [key] = slot
+        
+        var snapshot = sd.snapshots [key] ?? Snapshot ()
+        snapshot.lastDeaths = Array (r [r.count-n..<r.count].map { Int ($0)! })
+        sd.snapshots [key] = snapshot
+    }
+    
+    for r in fcases.enumeratedRows {
+        let key = r [4]
+        if key == "" {
+            continue
+        }
+
+        var snapshot = sd.snapshots [key]!
+        snapshot.lastConfirmed = Array (r [r.count-n..<r.count].map { Int ($0)! })
+        sd.snapshots [key] = snapshot
+    }
+}
+
+func validateUS (_ fdeaths: CSV, _ fcases: CSV)
+{
+    // Validate that the columns match, to catch any ambiguities on the data
+    if fdeaths.enumeratedColumns.count != fcases.enumeratedColumns.count+1{
+        print ("The tables do not match, deaths is supposed to have an additional column")
+        abort ()
+    }
+    let ccases = fcases.enumeratedColumns
+    let cdeathts = fdeaths.enumeratedColumns
+    for i in 0..<fcases.enumeratedColumns.count-11 {
+        let hcase = ccases [i+11].header
+        let hdeath = cdeathts [i+12].header
+        if hcase != hdeath {
+            print ("Different columns at \(i) \(hcase) and \(hdeath)")
+            abort ()
+        }
+    }
+    print ("Columns match")
+}
+
+func populateDeathWorld (slot: inout TrackedLocation, row: [String])
+{
+    //slot.admin = ""
+    slot.proviceState = row [0]
+    slot.countryRegion = row [1]
+    slot.lat = row [2]
+    slot.long = row [3]
+}
+
+func extractWorld (_ n: Int, _ fdeaths: CSV, _ fcases: CSV)
+{
+    // Now populate the data
+    for r in fdeaths.enumeratedRows {
+        let key = r [0] + "," + r [1]
+        
+        var slot = gd.globals [key] ?? TrackedLocation ()
+        populateDeathWorld(slot: &slot, row: r)
+        gd.globals [key] = slot
+        
+        var snapshot = sd.snapshots [key] ?? Snapshot ()
+        snapshot.lastDeaths = Array (r [r.count-n..<r.count].map { Int($0)! })
+        sd.snapshots [key] = snapshot
+    }
+    
+    for r in fcases.enumeratedRows {
+        let key = r [0] + "," + r [1]
+
+        var snapshot = sd.snapshots [key]!
+        snapshot.lastConfirmed = Array (r [r.count-n..<r.count].map { Int($0)! })
+        sd.snapshots [key] = snapshot
+    }
+}
+
+func saveData ()
+{
+    let encoder = JSONEncoder()
+    encoder.dateEncodingStrategy = .iso8601
+    encoder.outputFormatting = .prettyPrinted
+    let global = try! encoder.encode(gd)
+    try! global.write(to: URL(fileURLWithPath: "/tmp/global"))
+    
+    let data = try! encoder.encode (sd)
+    try! data.write (to: URL(fileURLWithPath: "/tmp/individual"))
+}
+
+// validate that we got data for everything
+func validateData ()
+{
+    for (_, slot) in sd.snapshots {
+        if slot.lastConfirmed.count != slot.lastDeaths.count {
+            print ("They do not have data")
+        }
+    }
+}
+
+let prefix = "/Users/miguel/cvs/"
+let deaths = "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+let cases = "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+let deathsWorld = "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+let casesWorld = "COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+
+let days = 20
+var fdeaths = try! CSV(url: URL(fileURLWithPath: prefix + deaths))
+var fcases = try! CSV(url: URL(fileURLWithPath: prefix + cases))
+
+gd = GlobalData()
+sd = SnapshotData()
+validateUS(fdeaths, fcases)
+
+extractUS (days, fdeaths, fcases)
+
+fdeaths = try! CSV(url: URL(fileURLWithPath: prefix + deathsWorld))
+fcases = try! CSV(url: URL(fileURLWithPath: prefix + casesWorld))
+extractWorld (days, fdeaths, fcases)
+
+validateData ()
+
+saveData()
